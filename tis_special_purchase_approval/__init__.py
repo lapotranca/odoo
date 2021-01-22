fa3f3aba4d3839ade9195560e18ed2e0e72f6069
lapotranca <71958823+lapotranca@users.noreply.github.com>
2021-01-21 18:52:55 -0600

    Add files via upload

---
+++ tis_special_purchase_approval/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# This module and its content is copyright of Technaureus Info Solutions Pvt. Ltd.
+# - © Technaureus Info Solutions Pvt. Ltd 2019. All rights reserved.
+
+from . import models
---
+++ tis_special_purchase_approval/models/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# This module and its content is copyright of Technaureus Info Solutions Pvt. Ltd.
+# - © Technaureus Info Solutions Pvt. Ltd 2019. All rights reserved.
+
+from . import product_template
+from . import purchase_order
