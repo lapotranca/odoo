24201286ff75409f7f178e3666657f1e50c16bd4
lapotranca <71958823+lapotranca@users.noreply.github.com>
2021-01-30 10:38:10 -0600

    Add files via upload

---
+++ as_financial_sheet_accounting/models/account_coa_report.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from odoo import models, api, _, fields
+
+class AccountChartOfAccountReport(models.AbstractModel):
+    _inherit = "account.coa.report"
+
+    filter_v_cost_centers = []
+    filter_v_cost_center = True
+    filter_v_departments = []
+    filter_v_department = True    
+    filter_v_region_reports = []
+    filter_v_region_report = True
+
