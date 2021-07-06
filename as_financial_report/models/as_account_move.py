d9065a10c6cbb70e5076d3cd0a89a5bb8d3ba8f0
phoenixsampras <phoenixsampras@gmail.com>
2021-07-06 12:16:22 -0400

    LaPotranca 33.0

--- as_financial_report/models/as_account_move.py
+++ as_financial_report/models/as_account_move.py
@@ -17,6 +17,37 @@ class account_move_line(models.Model):
     group_ediat_account_posted = fields.Boolean(compute='as_get_edit_account')
     state = fields.Selection(related="move_id.state")
 
+    @api.onchange('regiones_id')
+    def compute_regiones(self):
+        for rec in self:
+            return {'domain': {
+                'cost_center_id': [('id', 'in', rec.regiones_id.center_ids.ids)]
+            }}
+
+    @api.onchange('cost_center_id')
+    def compute_department(self):
+        for rec in self:
+            return {'domain': {
+                'department_id': [('id', 'in', rec.cost_center_id.department_ids.ids)]
+            }}
+
+
+    def _check_reconcile_validity(self):
+        #Perform all checks on lines
+        company_ids = set()
+        all_accounts = []
+        for line in self:
+            company_ids.add(line.company_id.id)
+            all_accounts.append(line.account_id)
+            if (line.matched_debit_ids or line.matched_credit_ids) and line.reconciled:
+                raise UserError(_('You are trying to reconcile some entries that are already reconciled.'))
+        if len(company_ids) > 1:
+            raise UserError(_('To reconcile the entries company should be the same for all entries.'))
+        # if len(set(all_accounts)) > 1:
+        #     raise UserError(_('Entries are not from the same account.'))
+        if not (all_accounts[0].reconcile or all_accounts[0].internal_type == 'liquidity'):
+            raise UserError(_('Account %s (%s) does not allow reconciliation. First change the configuration of this account to allow it.') % (all_accounts[0].name, all_accounts[0].code))
+        
     @api.onchange('debit','credit')
     @api.depends('debit','credit')
     def as_get_edit_account(self):
@@ -222,67 +253,76 @@ class account_move(models.Model):
         usuario = self.env.user
         if self.type in ('out_invoice','out_refund'):
             for move_line in self.invoice_line_ids:
-                move_line.analytic_tag_ids = usuario.analytic_tag_ids.ids
-                move_line.regiones_id = usuario.regiones_id.id
-                move_line.cost_center_id = usuario.cost_center_id.id
-                move_line.department_id = usuario.departmento_id.id
+                if not move_line.regiones_id or not move_line.cost_center_id or not move_line.department_id:
+                    move_line.analytic_tag_ids = usuario.analytic_tag_ids.ids
+                    move_line.regiones_id = usuario.regiones_id.id
+                    move_line.cost_center_id = usuario.cost_center_id.id
+                    move_line.department_id = usuario.departmento_id.id
             for move_line in self.line_ids:
-                move_line.analytic_tag_ids = usuario.analytic_tag_ids.ids
-                move_line.regiones_id = usuario.regiones_id.id
-                move_line.cost_center_id = usuario.cost_center_id.id
-                move_line.department_id = usuario.departmento_id.id
+                if not move_line.regiones_id or not move_line.cost_center_id or not move_line.department_id:
+                    move_line.analytic_tag_ids = usuario.analytic_tag_ids.ids
+                    move_line.regiones_id = usuario.regiones_id.id
+                    move_line.cost_center_id = usuario.cost_center_id.id
+                    move_line.department_id = usuario.departmento_id.id
         elif self.as_employee_id:
             for move_line in self.line_ids:
-                move_line.analytic_tag_ids = self.as_employee_id.analytic_tag_ids.ids
-                move_line.regiones_id = self.as_employee_id.regiones_purchase_id.id
-                move_line.cost_center_id = self.as_employee_id.cost_purchase_center_id.id
-                move_line.department_id = self.as_employee_id.department_puechase_id.id
+                if not move_line.regiones_id or not move_line.cost_center_id or not move_line.department_id:
+                    move_line.analytic_tag_ids = self.as_employee_id.analytic_tag_ids.ids
+                    move_line.regiones_id = self.as_employee_id.regiones_purchase_id.id
+                    move_line.cost_center_id = self.as_employee_id.cost_purchase_center_id.id
+                    move_line.department_id = self.as_employee_id.department_puechase_id.id
         else:
             for move_line in self.invoice_line_ids:
-                if move_line.payment_id:
-                    if move_line.payment_id.payment_type in ('inbound'):
-                        move_line.analytic_tag_ids = usuario.analytic_tag_ids.ids
-                        move_line.regiones_id = usuario.regiones_id.id
-                        move_line.cost_center_id = usuario.cost_center_id.id
-                        move_line.department_id = usuario.departmento_id.id
-                    else:
+                if not move_line.regiones_id or not move_line.cost_center_id or not move_line.department_id:
+                    if move_line.payment_id:
+                        if move_line.payment_id.payment_type in ('inbound'):
+                            move_line.analytic_tag_ids = usuario.analytic_tag_ids.ids
+                            move_line.regiones_id = usuario.regiones_id.id
+                            move_line.cost_center_id = usuario.cost_center_id.id
+                            move_line.department_id = usuario.departmento_id.id
+                        else:
+                            move_line.analytic_tag_ids = usuario.analytic_tag_purchase_ids.ids
+                            move_line.regiones_id = usuario.regiones_purchase_id.id
+                            move_line.cost_center_id = usuario.cost_purchase_center_id.id
+                            move_line.department_id = usuario.department_puechase_id.id                        
+                    else:  
                         move_line.analytic_tag_ids = usuario.analytic_tag_purchase_ids.ids
                         move_line.regiones_id = usuario.regiones_purchase_id.id
                         move_line.cost_center_id = usuario.cost_purchase_center_id.id
-                        move_line.department_id = usuario.department_puechase_id.id                        
-                else:  
-                    move_line.analytic_tag_ids = usuario.analytic_tag_purchase_ids.ids
-                    move_line.regiones_id = usuario.regiones_purchase_id.id
-                    move_line.cost_center_id = usuario.cost_purchase_center_id.id
-                    move_line.department_id = usuario.department_puechase_id.id
+                        move_line.department_id = usuario.department_puechase_id.id
             for move_line in self.line_ids:
                 if move_line.payment_id:
                     if move_line.payment_id.payment_type in ('inbound'):
-                        move_line.analytic_tag_ids = usuario.analytic_tag_ids.ids
-                        move_line.regiones_id = usuario.regiones_id.id
-                        move_line.cost_center_id = usuario.cost_center_id.id
-                        move_line.department_id = usuario.departmento_id.id
+                        if not move_line.regiones_id or not move_line.cost_center_id or not move_line.department_id:
+                            move_line.analytic_tag_ids = usuario.analytic_tag_ids.ids
+                            move_line.regiones_id = usuario.regiones_id.id
+                            move_line.cost_center_id = usuario.cost_center_id.id
+                            move_line.department_id = usuario.departmento_id.id
                     else:
+                        if not move_line.regiones_id or not move_line.cost_center_id or not move_line.department_id:
+                            move_line.analytic_tag_ids = usuario.analytic_tag_purchase_ids.ids
+                            move_line.regiones_id = usuario.regiones_purchase_id.id
+                            move_line.cost_center_id = usuario.cost_purchase_center_id.id
+                            move_line.department_id = usuario.department_puechase_id.id                        
+                else: 
+                    if not move_line.regiones_id or not move_line.cost_center_id or not move_line.department_id:
                         move_line.analytic_tag_ids = usuario.analytic_tag_purchase_ids.ids
                         move_line.regiones_id = usuario.regiones_purchase_id.id
                         move_line.cost_center_id = usuario.cost_purchase_center_id.id
-                        move_line.department_id = usuario.department_puechase_id.id                        
-                else: 
-                    move_line.analytic_tag_ids = usuario.analytic_tag_purchase_ids.ids
-                    move_line.regiones_id = usuario.regiones_purchase_id.id
-                    move_line.cost_center_id = usuario.cost_purchase_center_id.id
-                    move_line.department_id = usuario.department_puechase_id.id
+                        move_line.department_id = usuario.department_puechase_id.id
         if self.as_extract_sale:
             for move_line in self.invoice_line_ids:
-                move_line.analytic_tag_ids = usuario.analytic_tag_ids.ids
-                move_line.regiones_id = usuario.regiones_id.id
-                move_line.cost_center_id = usuario.cost_center_id.id
-                move_line.department_id = usuario.departmento_id.id
+                if not move_line.regiones_id or not move_line.cost_center_id or not move_line.department_id:
+                    move_line.analytic_tag_ids = usuario.analytic_tag_ids.ids
+                    move_line.regiones_id = usuario.regiones_id.id
+                    move_line.cost_center_id = usuario.cost_center_id.id
+                    move_line.department_id = usuario.departmento_id.id
             for move_line in self.line_ids:
-                move_line.analytic_tag_ids = usuario.analytic_tag_ids.ids
-                move_line.regiones_id = usuario.regiones_id.id
-                move_line.cost_center_id = usuario.cost_center_id.id
-                move_line.department_id = usuario.departmento_id.id
+                if not move_line.regiones_id or not move_line.cost_center_id or not move_line.department_id:
+                    move_line.analytic_tag_ids = usuario.analytic_tag_ids.ids
+                    move_line.regiones_id = usuario.regiones_id.id
+                    move_line.cost_center_id = usuario.cost_center_id.id
+                    move_line.department_id = usuario.departmento_id.id
         return super(account_move, self).write(vals)
 
     def action_post(self):
